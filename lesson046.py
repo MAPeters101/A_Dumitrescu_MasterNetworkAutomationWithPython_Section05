3ffc3368e310431e57cc4ee1dea752ce4b91b98e
Mark Peters <mapeters101@earthlink.net>
2024-11-18 14:04:26 -0500

    Lesson048:  Work on lesson.

--- lesson046.py
+++ lesson046.py
@@ -5,7 +5,7 @@
 import pickle
 
 # Declaring a dictionary
-friends = {"Dan": (20, "London", 13242252), "Maria": [25, "Madrid", 34232424]}
+friends = {"Dan": [20, "London", 13242252], "Maria": [25, "Madrid", 34232424]}
 
 # Serializing the dictionary to binary file called `friends.dat`
 with open('friends.dat', 'wb') as f:  # b -> binary mode
--- lesson047.py
+++ lesson047.py
@@ -1,7 +1,7 @@
 import json
-friends = {"Dan": [20, "London", 3234342], "Maria": [25, "Madrid", 43525222]}
+friends = {"Dan": (20, "London", 3234342), "Maria": (25, "Madrid", 43525222)}
 
-with open('friends.json', 'w') as f:
+with open('friends_tuple.json', 'w') as f:
     json.dump(friends, f, indent=4)
 
 json_string = json.dumps(friends, indent=4)
